be1da68d95962e1fc3badb4a6b37e648a028200b
Mateusz Adamiak <amalgammatt@gmail.com>
2012-10-29 14:27:09 +0100

    organizacja projektu

---
+++ Program/ActEditor/test/pl/wroc/pwr/student/acteditor/TesterTest.java
@@ -0,0 +1,14 @@
+package pl.wroc.pwr.student.acteditor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesterTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
