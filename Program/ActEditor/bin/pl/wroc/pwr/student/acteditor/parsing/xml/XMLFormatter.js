c171b164fa1973e3d63d163d31f1d959f4a9bdc4
Mateusz Adamiak <amalgammatt@gmail.com>
2012-10-31 12:02:42 +0100

    .

--- Program/ActEditor/bin/pl/wroc/pwr/student/acteditor/parsing/xml/XMLFormatter.js
+++ Program/ActEditor/bin/pl/wroc/pwr/student/acteditor/parsing/xml/XMLFormatter.js
@@ -1,7 +1,4 @@
 ﻿Clazz.declarePackage ("pl.wroc.pwr.student.acteditor.parsing.xml");
 Clazz.load (["pl.wroc.pwr.student.acteditor.parsing.Formatter"], "pl.wroc.pwr.student.acteditor.parsing.xml.XMLFormatter", null, function () {
-c$ = Clazz.decorateAsClass (function () {
-this.xmlContent = null;
-Clazz.instantialize (this, arguments);
-}, pl.wroc.pwr.student.acteditor.parsing.xml, "XMLFormatter", null, pl.wroc.pwr.student.acteditor.parsing.Formatter);
+c$ = Clazz.declareType (pl.wroc.pwr.student.acteditor.parsing.xml, "XMLFormatter", null, pl.wroc.pwr.student.acteditor.parsing.Formatter);
 });
--- Program/ActEditor/src/pl/wroc/pwr/student/acteditor/parsing/xml/XMLFormatter.java
+++ Program/ActEditor/src/pl/wroc/pwr/student/acteditor/parsing/xml/XMLFormatter.java
@@ -3,5 +3,5 @@ package pl.wroc.pwr.student.acteditor.parsing.xml;
 import pl.wroc.pwr.student.acteditor.parsing.Formatter;
 
 public class XMLFormatter implements Formatter {
-	private String xmlContent;
+//	private String xmlContent;
 }
